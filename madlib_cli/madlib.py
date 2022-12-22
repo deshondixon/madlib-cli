90e0ef014d8f0070319631f2dff2a84879c46dbb
deshondixon <deshondixon@gmail.com>
2022-12-21 23:22:13 -0800

    read template passes and filenotfound passes

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -9,9 +9,13 @@ print("""
     """)
 
 
-def read_template():
-    with open() as f:
-        return f.read()
+def read_template(temp):
+    try:
+        with open(temp) as f:
+            contents = f.read()
+            return contents
+    except FileNotFoundError:
+        raise FileNotFoundError
 
 
 def parse_template():
