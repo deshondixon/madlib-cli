9adf51771b84568687c5f615873c47c92ea1a13a
deshondixon <deshondixon@gmail.com>
2022-12-21 22:25:54 -0800

    intro message created

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,10 +1,24 @@
-welcome = """
+print("""
     **************************************
     **  Welcome to Madlib CLI Edition!  **
+    **          as they appear          **
+    **   Type in adjectives and nouns   **
+    **                                  **
+    ** To quit at any time, type "quit" **
     **************************************
-    """
-
-# def read_template():
+    """)
 
 
-# def parse_template():
+def read_template(string):
+    with open(string, mode='r') as f:
+        return f.read()
+
+
+def parse_template():
+    with open() as f:
+        return f.read()
+
+
+def merge():
+    with open() as f:
+        return f.read()
